33e4a207724dbfe7d91d783604a6d61751fa852b
jamal.osman <jamal.osman@cohaesus.co.uk>
2017-01-10 09:53:00 +0000

    scroll view working

--- DevSandbox/PlaygroundViewController.cs
+++ DevSandbox/PlaygroundViewController.cs
@@ -9,19 +9,8 @@ namespace DevSandbox
     public class PlaygroundViewController : UIViewController
     {
         public UIScrollView ScrollContainer { get; set; }
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            //View.LayoutMargins = new UIEdgeInsets(10, 10, 10, 10);
-            View.BackgroundColor = UIColor.White;
-            View.ClipsToBounds = true;
-
-
-            ScrollContainer = new UIScrollView(View.Frame);
-            ScrollContainer.BackgroundColor = UIColor.White;
-            ScrollContainer.TranslatesAutoresizingMaskIntoConstraints = false;
-
-            var views = new LinkedList<UIView>(
+        public UIView ContentView { get; set; } 
+        public LinkedList<UIView> RowViews { get; set; } = new LinkedList<UIView>(
                 new List<UIView>{
                     new UITextView
                     {
@@ -34,7 +23,7 @@ namespace DevSandbox
                         Text = "Subheadline",
                         BackgroundColor = UIColor.DarkGray,
                         Font = UIFont.PreferredSubheadline
-                    },
+},
                     new UITextView
                     {
                         Text = "Title 1",
@@ -63,12 +52,34 @@ namespace DevSandbox
                 }
             );
 
-            View.AddSubviews(views.ToArray());
+        public PlaygroundViewController()
+        {
+            ContentView = new UIView();
+            ContentView.BackgroundColor = UIColor.FromRGB(60, 0, 0);
+            ContentView.TranslatesAutoresizingMaskIntoConstraints = false;
 
-            var node = views.First;
+            ScrollContainer = new UIScrollView();
+            ScrollContainer.BackgroundColor = UIColor.FromRGB(0, 60, 0);
+            ScrollContainer.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            View.BackgroundColor = UIColor.White;
+            //View.TranslatesAutoresizingMaskIntoConstraints = false;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            ContentView.AddSubviews(RowViews.ToArray());
+
+            ScrollContainer.Add(ContentView);
+
+            View.Add(ScrollContainer);
+
+
+            var node = RowViews.First;
             while (node.Next != null)
             {
-                NSObject refItem = node.Previous?.Value ?? new NSObject(TopLayoutGuide.Handle);
+                NSObject refItem = node.Previous?.Value ?? ContentView;
                 var refAttribute = node.Previous == null ? NSLayoutAttribute.Top : NSLayoutAttribute.Bottom;
                 UIView subview = node.Value;
                 subview.TranslatesAutoresizingMaskIntoConstraints = false;
@@ -78,27 +89,31 @@ namespace DevSandbox
                 //subview.TrailingAnchor.ConstraintEqualTo(View.TrailingAnchor, 5).Active = true;
                 //subview.HeightAnchor.ConstraintEqualTo(80);
 
-                View.AddConstraints(new NSLayoutConstraint[] {
+                ContentView.AddConstraints(new NSLayoutConstraint[] {
                     NSLayoutConstraint.Create(subview,NSLayoutAttribute.Top,NSLayoutRelation.Equal,  refItem, refAttribute,1,20),
-                    NSLayoutConstraint.Create(subview,NSLayoutAttribute.Leading,NSLayoutRelation.Equal, View, NSLayoutAttribute.Leading,1,10),
-                    NSLayoutConstraint.Create(subview,NSLayoutAttribute.Trailing,NSLayoutRelation.Equal, View ,NSLayoutAttribute.Trailing,1,-10),
+                    NSLayoutConstraint.Create(subview,NSLayoutAttribute.Leading,NSLayoutRelation.Equal, ContentView, NSLayoutAttribute.Leading,1,10),
+                    NSLayoutConstraint.Create(subview,NSLayoutAttribute.Trailing,NSLayoutRelation.Equal, ContentView ,NSLayoutAttribute.Trailing,1,-10),
                     NSLayoutConstraint.Create(subview,NSLayoutAttribute.Height, NSLayoutRelation.Equal, null, NSLayoutAttribute.NoAttribute, 1, 80),
                 });
 
                 node = node.Next;
             }
 
-            //View.Add(ScrollContainer);
-            //View.AddConstraints(new NSLayoutConstraint[] {
-            //    NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Top,NSLayoutRelation.Equal, View, NSLayoutAttribute.Top,1,0),
-            //    NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Bottom,NSLayoutRelation.Equal, View ,NSLayoutAttribute.Bottom,1,0),
-            //    NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Leading,NSLayoutRelation.Equal, View, NSLayoutAttribute.Leading,1,0),
-            //    NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Trailing,NSLayoutRelation.Equal, View ,NSLayoutAttribute.Trailing,1,0),
-            //});
+            ScrollContainer.AddConstraints(new NSLayoutConstraint[] {
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Top,NSLayoutRelation.Equal, ScrollContainer, NSLayoutAttribute.Top,1,0),
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Bottom,NSLayoutRelation.Equal, ScrollContainer ,NSLayoutAttribute.Bottom,1,0),
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Leading,NSLayoutRelation.Equal, ScrollContainer, NSLayoutAttribute.Leading,1,0),
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Trailing,NSLayoutRelation.Equal, ScrollContainer ,NSLayoutAttribute.Trailing,1,0),
+            });
 
-            //ScrollContainer.BackgroundColor = UIColor.White;
-            //View.Add(ScrollContainer);
-            
+            View.AddConstraints(new NSLayoutConstraint[] {
+                NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Top,NSLayoutRelation.Equal, TopLayoutGuide, NSLayoutAttribute.Top,1,0),
+                NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Bottom,NSLayoutRelation.Equal, BottomLayoutGuide, NSLayoutAttribute.Bottom,1,0),
+                NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Leading,NSLayoutRelation.Equal, View, NSLayoutAttribute.Leading,1,0),
+                NSLayoutConstraint.Create(ScrollContainer,NSLayoutAttribute.Trailing,NSLayoutRelation.Equal, View ,NSLayoutAttribute.Trailing,1,0),
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Width,NSLayoutRelation.Equal, View, NSLayoutAttribute.Width,1,0),
+                NSLayoutConstraint.Create(ContentView,NSLayoutAttribute.Height,NSLayoutRelation.Equal, View, NSLayoutAttribute.Height,1,0),
+            });
         }
     }
 }
